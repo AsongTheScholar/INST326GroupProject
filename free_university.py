356c21015ecfc064e1b0a65f18cd9da959d747a9
AsongTheScholar <129445178+AsongTheScholar@users.noreply.github.com>
2023-04-30 03:45:07 +0000

    Brought in the Course object and seach function we created

--- free_university.py
+++ free_university.py
@@ -1,19 +1,16 @@
-from selenium import webdriver
+import requests
+from time import sleep
 import pandas as pd
+from selenium import webdriver
 
-catalogs = {"Harvard":"https://pll.harvard.edu/catalog/free?page=0", "MIT":"https://ocw.mit.edu/search/", "UMD":"https://umd.catalog.instructure.com/?filters%5B%5D=free", "Open":"https://www.open.edu/openlearn/free-courses/full-catalogue"}
+class Course():
+    def __init__(self, course_name, title, course_credits, genEdSatisfactions):
+        self.course_name = course_name
+        self.title = title
+        self.course_credits = course_credits
+        self.genEdSatisfactions = genEdSatisfactions
 
-def searchUni(frame, university, tags):
-    '''Starts a parse query for free courses.
-        Args:
-            frame (str) - Dataframe to write to.
-            university (str) - Catalog to parse through.
-            tags (list) - Tags to apply to refine the search.
-        Returns:
-            TBD; likely the dataframe or none.'''
-
-#The follwoign are functions to search particular catalogs, in case the html formatting is too distinct to handle all in one function
-def parseHarvard(frame, tags):
+def search(courseTag):
     '''Starts a parse query for free courses.
         Args:
             frame (str) - Dataframe to write to.
@@ -21,30 +18,23 @@ def parseHarvard(frame, tags):
         Returns:
             TBD; likely the dataframe or none.'''
 
-def parseMIT(frame, tags):
-    '''Starts a parse query for free courses.
-        Args:
-            frame (str) - Dataframe to write to.
-            tags (list) - Tags to apply to refine the search.
-        Returns:
-            TBD; likely the dataframe or none.'''
+    driver = webdriver.Chrome()
+    driver.get("https://app.testudo.umd.edu/soc/")
+    courses = driver.find_element("id","course-id-input")
+    courses.send_keys(courseTag)
+    search = driver.find_element("id","search-button")
+    search.click()
 
-def parseUMD(frame, tags):
-    '''Starts a parse query for free courses.
-        Args:
-            frame (str) - Dataframe to write to.
-            tags (list) - Tags to apply to refine the search.
-        Returns:
-            TBD; likely the dataframe or none.'''
+    page = driver.current_url
 
-def parseOpen(frame, tags):
-    '''Starts a parse query for free courses.
-        Args:
-            frame (str) - Dataframe to write to.
-            tags (list) - Tags to apply to refine the search.
-        Returns:
-            TBD; likely the dataframe or none.'''
+    try: 
+        pageText = requests.get(page).text
+        sleep(2)
+    except:
+        print("too fast!")
 
+    return pageText
+    
 def formatTable(frame, maxRows, sort):
     '''Takes a data frame and returns it in a more deliverable form.
         Args:
